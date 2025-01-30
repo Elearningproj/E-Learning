8273a9cf75e429eb2654458e1a7c9b1b8539ea8b
KD3_86711_rounsely <84245724+rnegi12@users.noreply.github.com>
2025-01-30 11:47:39 +0530

    Delete Frontend directory

--- Frontend/StudentPanel/src/service/config.js
+++
@@ -1,4 +0,0 @@
-export const config = {
-    serverUrl: 'http://localhost:4100',
-  }
-  
--- Frontend/StudentPanel/src/utils.js
+++
@@ -1,5 +0,0 @@
-import { config } from './service/config'
-
-export function createUrl(path) {
-  return `${config.serverUrl}/${path}`
-}
